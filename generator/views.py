29f4e3dbb47c63b5521e490c1a964ac811e0f0a0
Dallas Clarke <65244012+dallasclarke@users.noreply.github.com>
2022-01-08 14:59:56 -0800

    Able to generate random password for user.

--- generator/views.py
+++ generator/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 
 def home(req):
-    return render(req, "generator/home.html", {"password": "fd3920fds"})
+    return render(req, "generator/home.html")
 
 def password(req):
-    return render(req, "generator/password.html")
+    thepassword = ""
+    characters = list("abdcdefghijklmnopqrstuvwxyz")
+
+    if req.GET.get("uppercase"):
+        characters.extend(list("ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+
+    if req.GET.get("numbers"):
+        characters.extend("0123456789")
+
+    if req.GET.get("special"):
+        characters.extend(list("!@#$%^&*()"))
+
+    length = int(req.GET.get("length", 12))
+
+    for x in range(length):
+        thepassword += random.choice(characters)
+
+    return render(req, "generator/password.html", {"password": thepassword})
